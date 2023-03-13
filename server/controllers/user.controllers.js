ebc1b093a9c0087f8ada4687f7dd8ad4564c096c
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-03-13 20:53:28 +0700

    ok

--- server/controllers/user.controllers.js
+++ server/controllers/user.controllers.js
@@ -1,33 +1,31 @@
-const { RegisterUser_se } = require('../services/user.services');
+const { RegisterUser_service } = require("../services/user.services");
 
 // @route POST api/users
 // @desc Register user
-// @access Public 
+// @access Public
 
 const RegisterUser_co = async (req, res) => {
-    const { name, email, password, userRole, userImage } = req.body;
+  const { name, email, password, userRole, userImage } = req.body;
 
-    const user = { name, email, password, userRole, userImage }
+  const user = { name, email, password, userRole, userImage };
 
-    try {
-        // Call service
-        const result = await RegisterUser_se(user);
+  try {
+    // Call service
+    const result = await RegisterUser_service(user);
 
-        // Return result
-        const { status, success, message, content } = result;
-        if (!success) {
-            return res.status(status).send({ success, message });
-        } else {
-            return res.status(status).send({ success, message, content });
-        }
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ success: false, message: 'Internal server error' });
+    // Return result
+    const { status, success, message, content } = result;
+    if (!success) {
+      return res.status(status).send({ success, message });
+    } else {
+      return res.status(status).send({ success, message, content });
     }
-}
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ success: false, message: "Internal server error" });
+  }
+};
 
 module.exports = {
-    RegisterUser_co
-}
+  RegisterUser_co,
+};
