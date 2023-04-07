500559a13e75c6ed4fda28444bdba3380c7b62a7
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-04-07 09:40:45 +0700

    up

--- server/middlewares/validations/post.validation.js
+++ server/middlewares/validations/post.validation.js
@@ -2,7 +2,7 @@ const STATUS_CODE = require("../../util/SettingSystem");
 
 const post_checkEmpty = (req, res, next) => {
   // get accessToken from header
-  const accessToken = req.header("Bearer");
+  const accessToken = req.header("Bearer").split(" ")[1].replace(/"/g, "");
   const { title, content } = req.body;
 
   // Simple validation
