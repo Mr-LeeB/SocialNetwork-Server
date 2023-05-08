09803864a2d1e2c14ce6e915078e262b8cf197c6
Nguyễn Hoàng Hải <ln26805@gmail.com>
2023-05-08 19:32:18 +0700

    done trigger realtime

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cookieParser());
 app.use(fileUpload());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     origin: 'http://127.0.0.1:3000',
