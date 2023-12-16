18e841e3d4ea26481b736f35c846878823f7845b
ZLoganZ <ln26805@gmail.com>
2023-12-16 18:08:25 +0700

    Update MongoDB connection string in conversation.test.js

--- server/tests/conversation.test.js
+++ server/tests/conversation.test.js
@@ -6,7 +6,7 @@ const app = require('../app');
 /* Connect to the database before tests */
 beforeAll(async () => {
   await mongoose.connect(
-    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktjepad.mongodb.net/SocialNetworkTestAuth`,
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ktjepad.mongodb.net/SocialNetworkTestConversation`,
   );
 });
 
